274381e6ddd6f10bf5df9c0da8191d7cd4bcedc1
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-22 15:52:45 +0100

    Create iMessage Root on React Native Side

---
+++ index.message.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import AppMessageExtension from './App.iMessage';
+
+AppRegistry.registerComponent('AwesomeProjectMessageExtension', () => AppMessageExtension);

9f231284f2ba3cdfaceb11b4a5210be52b9e1686
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-25 16:13:27 +0100

    Update Xcode configuration

---
+++ ios/AwesomeProjectMessageExtension/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  AwesomeProject
+//
+
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>

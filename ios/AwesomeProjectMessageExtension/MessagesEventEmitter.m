5946a8e2901fc0ebcc109d1304b750cea655b41d
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-28 09:50:02 +0100

    Cleanup

--- ios/AwesomeProjectMessageExtension/MessagesEventEmitter.m
+++ ios/AwesomeProjectMessageExtension/MessagesEventEmitter.m
@@ -4,7 +4,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "React/RCTBridgeModule.h"
 #import "React/RCTEventEmitter.h"
 
 @interface RCT_EXTERN_MODULE(MessagesEventEmitter, RCTEventEmitter)
--- ios/AwesomeProjectMessageExtension/MessagesManager.m
+++ ios/AwesomeProjectMessageExtension/MessagesManager.m
@@ -13,14 +13,12 @@ RCT_EXTERN_METHOD(getActiveConversation: (RCTResponseSenderBlock)callback)
 RCT_EXTERN_METHOD(composeMessage:
                   (NSDictionary *)messageData
                   resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject
-                  )
+                  rejecter: (RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(getPresentationStyle: (RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(updatePresentationStyle:
                   (NSString *)style
                   resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject
-                  )
+                  rejecter: (RCTPromiseRejectBlock)reject)
 @end

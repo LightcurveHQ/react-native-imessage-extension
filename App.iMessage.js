8f5b9abcc9747ecee83627ac4b1494d49993c2f1
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-28 10:37:56 +0100

    Add presentationStyle update logic to iMessage App

--- App.iMessage.js
+++ App.iMessage.js
@@ -1,11 +1,50 @@
+//
+// App.iMessage.js
+//
+
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  NativeModules,
+  NativeEventEmitter,
+  Button,
+} from 'react-native';
+
+const { MessagesManager, MessagesEventEmitter } = NativeModules;
+const MessagesEvents = new NativeEventEmitter(MessagesEventEmitter);
 
 export default class App extends Component {
+  state = {
+    presentationStyle: '',
+  }
+
+  componentDidMount() {
+    MessagesManager
+      .getPresentationStyle(presentationStyle => this.setState({ presentationStyle }))
+
+    MessagesEvents
+      .addListener('onPresentationStyleChanged', ({ presentationStyle }) => this.setState({ presentationStyle }));
+  }
+
+  onTogglePresentationStyle = () => {
+    MessagesManager
+      .updatePresentationStyle(this.state.presentationStyle === 'expanded' ? 'compact' : 'expanded')
+      .then(presentationStyle => this.setState({ presentationStyle }))
+  }
+
   render() {
     return (
       <View>
-        <Text>Welcome to React Native iMessage Extension!</Text>
+        <Text>
+          Welcome to React Native iMessage Extension!
+        </Text>
+
+        <Button
+          title="Toggle the Presentation Style"
+          onPress={this.onTogglePresentationStyle}
+          disabled={!this.state.presentationStyle}
+        />
       </View>
     );
   }
